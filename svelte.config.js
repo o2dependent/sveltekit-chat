20f10b1b1c1187690b787b2e152c524785412eb0
Ethan Olsen <ethan@crowdhubapps.com>
2022-01-10 18:39:49 -0800

    feat: add path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,29 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		defaults: {
+			style: 'postcss'
+		},
+		postcss: true
+	}),
 
 	kit: {
 		adapter: adapter(),
+
+		vite: {
+			resolve: {
+				alias: {
+					$stores: path.resolve('./src/stores'),
+					$services: path.resolve('./src/services')
+				}
+			}
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
